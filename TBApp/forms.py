2cff4c2fcdbe596b2570d68b243ab7974bafe933
Nicholas Almeder <32600512+BaritoneBeard@users.noreply.github.com>
2020-11-10 14:40:34 -0500

    Rest of commit that didn't go through first time

---
+++ TBApp/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Account
+from .models import Task
+
+class AccountForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        exclude = ['profile_pic','bio']
+        widgets = {
+        'password': forms.PasswordInput(),
+    }
+''' Forms Needed:
+    New Task Form
+    Sign-In Form
+    Possibly Update Account Info Form
+'''
+class NewTaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        exclude = {}
+        #anything else?
+
+    
+
+
