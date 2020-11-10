61976547c33a87e0c23aa4c350231f1c3b06ce81
Nicholas Almeder <32600512+BaritoneBeard@users.noreply.github.com>
2020-11-10 14:31:02 -0500

    Added new task
    (Though incomplete, currently. Still runs/works)

--- TBApp/views.py
+++ TBApp/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 # from django.contrib.auth.decorators import login_required
 # from django.http import HttpResponseRedirect, HttpResponse
 # from django.contrib.auth import authenticate, login, logout
-
+from TBApp.forms import AccountForm
+from TBApp.forms import NewTaskForm
+from django import forms
 # Create your views here.
+
+
 def signIn(request):
     if request.method == 'POST':
         pass
@@ -12,8 +16,24 @@ def signIn(request):
 def home(request):
     return render(request,"TBApp/index.html")
 
+
 def signUp(request):
-    return render(request,"TBApp/signUp.html")
+    form = AccountForm()
+    errors = ""
+    if request.method == "POST":
+        profileForm = AccountForm(request.POST)
+        if profileForm.is_valid():
+            profileForm.save(commit=True)
+            return home(request)
+        else:
+            errors = profileForm.errors
+
+    return render(request,"TBApp/signUp.html",{'form':form, 'errors':errors})
 
 def profile(request):
     return render(request,"TBApp/profile.html")
+
+def testTask(request):
+    form = NewTaskForm
+    errors = ""
+    return render(request, "TBApp/NewTask.html")
