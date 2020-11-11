a4d3a2ac3a215bc2e8b15761b668b7177d335e59
Carlos Sousa <c.sousa4758@gmail.com>
2020-11-10 19:59:25 -0500

    set up sign-in page:

--- TBApp/urls.py
+++ TBApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns =[
-    path('sign-in',views.signIn, name="sign-In"),
-    path('sign-up',views.signUp,name="sign-up"),
-    path('profile',views.profile,name="profile"),
+    path('sign-in/',views.signIn, name="sign-In"),
+    path('sign-up/',views.signUp,name="sign-up"),
+    path('logout/',views.user_logout, name="logout"),
+    path('profile/',views.profile,name="profile"),
 ]
--- TBApp/views.py
+++ TBApp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-# from django.contrib.auth.decorators import login_required
-# from django.http import HttpResponseRedirect, HttpResponse
-# from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 from TBApp.forms import AccountForm
 from django import forms
 # Create your views here.
@@ -9,9 +10,26 @@ from django import forms
 
 def signIn(request):
     if request.method == 'POST':
-        pass
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username,password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('home'))
+        else:
+            loginAttempt = "Login failed, please Check the username and password and try again."
+            return render(request,"TBAPP/signIn.html",{'error':loginAttempt})
     return render(request,"TBApp/signIn.html")
 
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
+
+@login_required(login_url="/accounts/sign-in/")
 def home(request):
     return render(request,"TBApp/index.html")
 
@@ -23,11 +41,11 @@ def signUp(request):
         profileForm = AccountForm(request.POST)
         if profileForm.is_valid():
             profileForm.save(commit=True)
-            return home(request)
+            return HttpResponseRedirect(reverse('home'))
         else:
             errors = profileForm.errors
 
     return render(request,"TBApp/signUp.html",{'form':form, 'errors':errors})
-
+@login_required(login_url="/accounts/sign-in/")
 def profile(request):
     return render(request,"TBApp/profile.html")
